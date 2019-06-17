b4ef7a4128990518daf82fd4d4dd9ec0f771cae5
André Cruz <andremiguelcruz@msn.com>
2019-06-17 22:08:38 +0100

    feat: integrate idm-client

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IPFS from 'ipfs';
+import createIdmClient from 'idm-client';
+import { createClientSide } from 'idm-bridge-postmsg';
 import { create } from 'jss';
 import { StylesProvider, jssPreset } from '@material-ui/styles';
 import { configure } from './stores';
 import './index.css';
 import App from './App';
+
+const WALLET_URL = 'http://localhost:3000';
+const APP = {
+    name: 'Chat demo app',
+    homepageUrl: window.location.origin,
+    iconUrl: `${window.location.origin}/favicon.ico`,
+};
 
 const setup = async () => {
     // Setup IPFS
@@ -25,8 +34,12 @@ const setup = async () => {
         node.on('error', (err) => reject(err));
     });
 
+    // Setup IDM Client
+    const idmBridge = await createClientSide(WALLET_URL);
+    const idmClient = await createIdmClient(APP, idmBridge, { ipfs });
+
     // Finally configure our stores
-    await configure({ ipfs });
+    await configure({ ipfs, idmClient });
 };
 
 const jss = create({

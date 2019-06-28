1dcf085050ab26a2d43207cd91221a33eabe3cfa
André Cruz <andremiguelcruz@msn.com>
2019-06-28 23:25:20 +0200

    feat: integrate idm-client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IPFS from 'ipfs';
+import createIdmClient from 'idm-client';
+import { createClientSide } from 'idm-bridge-postmsg';
 import { create } from 'jss';
 import { StylesProvider, jssPreset } from '@material-ui/styles';
 import { configure } from './stores';
 import './index.css';
 import Boot from './Boot';
 import App from './App';
+
+const WALLET_URL = 'https://demo.nomios.io';
+const APP = {
+    name: 'Chat demo app',
+    homepageUrl: window.location.origin,
+    iconUrl: `${window.location.href}favicon.ico`,
+};
 
 const setup = async () => {
     // Setup IPFS
@@ -26,8 +35,12 @@ const setup = async () => {
         node.on('error', (err) => reject(err));
     });
 
+    // Setup IDM Client
+    const idmBridge = await createClientSide(WALLET_URL);
+    const idmClient = await createIdmClient(APP, idmBridge, { ipfs });
+
     // Finally configure our stores
-    await configure({ ipfs });
+    await configure({ ipfs, idmClient });
 
     return ipfs;
 };
